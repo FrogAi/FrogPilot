1ee1f82e1e1cd6a8b8c504ca4f29c6aae3b96ece
James <91348155+FrogAi@users.noreply.github.com>
2025-10-02 15:57:15 -0700

    Increase steering torque for Subaru Impreza

--- opendbc_repo/opendbc/car/subaru/values.py
+++ opendbc_repo/opendbc/car/subaru/values.py
@@ -26,6 +26,9 @@ class CarControllerParams:
     elif CP.carFingerprint == CAR.SUBARU_IMPREZA_2020:
       self.STEER_DELTA_UP = 35
       self.STEER_MAX = 1439
+    # FrogPilot variables
+    elif CP.carFingerprint == CAR.SUBARU_IMPREZA:
+      self.STEER_MAX = 3071
     else:
       self.STEER_MAX = 2047
 
