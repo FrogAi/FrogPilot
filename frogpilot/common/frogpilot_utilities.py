976e3cdfc78d33e2f6b4da03680dcee405f27423
James <91348155+FrogAi@users.noreply.github.com>
2025-10-03 13:53:16 -0700

    Curve Speed Controller
    Co-Authored-By: Jacob Pfeifer <jacob@pfeifer.dev>

--- frogpilot/common/frogpilot_utilities.py
+++ frogpilot/common/frogpilot_utilities.py
@@ -36,6 +36,20 @@ def run_thread_with_lock(name, target, args=(), report=True):
       running_threads[name] = thread
 
 
+# Credit goes to Pfeiferj!
+def calculate_road_curvature(modelData, v_ego):
+  orientation_rate = np.array(modelData.orientationRate.z)
+  velocity = np.array(modelData.velocity.x)
+  timebase = np.array(modelData.orientationRate.t)
+
+  lateral_acceleration = orientation_rate * velocity
+  index = np.argmax(np.abs(lateral_acceleration))
+  predicted_lateral_acc = float(lateral_acceleration[index])
+  time_to_curve = float(timebase[index])
+
+  return predicted_lateral_acc / max(v_ego, 1)**2, max(time_to_curve, 1)
+
+
 def clean_model_name(name):
   return (
     name.replace("🗺️", "")
