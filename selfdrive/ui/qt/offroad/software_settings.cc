893db69f3a0bdab804fbbfe74871212760ec4961
James <91348155+FrogAi@users.noreply.github.com>
2025-10-02 22:29:06 -0700

    Block users from using development branches

--- selfdrive/ui/qt/offroad/software_settings.cc
+++ selfdrive/ui/qt/offroad/software_settings.cc
@@ -61,6 +61,11 @@ SoftwarePanel::SoftwarePanel(QWidget* parent) : ListWidget(parent) {
   connect(targetBranchBtn, &ButtonControl::clicked, [=]() {
     auto current = params.get("GitBranch");
     QStringList branches = QString::fromStdString(params.get("UpdaterAvailableBranches")).split(",");
+    if (!frogpilotUIState()->frogpilot_toggles.value("frogs_go_moo").toBool()) {
+      branches.removeAll("FrogPilot-Development");
+      branches.removeAll("FrogPilot-Vetting");
+      branches.removeAll("MAKE-PRS-HERE");
+    }
     for (QString b : {current.c_str(), "devel-staging", "devel", "nightly", "nightly-dev", "master"}) {
       auto i = branches.indexOf(b);
       if (i >= 0) {
