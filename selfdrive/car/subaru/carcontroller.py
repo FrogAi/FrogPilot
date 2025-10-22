8d295aa6158e02227f94e7204a6c3da14a0ab426
James <91348155+FrogAi@users.noreply.github.com>
2025-10-22 15:08:41 -0700

    Remove unused import from carcontroller.py

--- selfdrive/car/subaru/carcontroller.py
+++ selfdrive/car/subaru/carcontroller.py
@@ -3,7 +3,7 @@ from opendbc.can.packer import CANPacker
 from openpilot.selfdrive.car import apply_driver_steer_torque_limits, common_fault_avoidance
 from openpilot.selfdrive.car.interfaces import CarControllerBase
 from openpilot.selfdrive.car.subaru import subarucan
-from openpilot.selfdrive.car.subaru.values import DBC, GLOBAL_ES_ADDR, GLOBAL_GEN2, HYBRID_CARS, PREGLOBAL_CARS, CanBus, CarControllerParams, SubaruFlags, SubaruFrogPilotFlags
+from openpilot.selfdrive.car.subaru.values import DBC, GLOBAL_ES_ADDR, GLOBAL_GEN2, HYBRID_CARS, PREGLOBAL_CARS, CanBus, CarControllerParams, SubaruFlags
 
 # FIXME: These limits aren't exact. The real limit is more than likely over a larger time period and
 # involves the total steering angle change rather than rate, but these limits work well for now
