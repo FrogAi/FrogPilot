PeterPhuTran/openpilot

FrogPilot structs

selfdrive/car/card.py
@@ -5,7 +5,7 @@ import threading
 
 import cereal.messaging as messaging
 
-from cereal import car, log
+from cereal import car, custom, log
 
 from openpilot.common.params import Params
 from openpilot.common.realtime import config_realtime_process, Priority, Ratekeeper
@@ -19,6 +19,8 @@ from opendbc.car.car_helpers import get_car, interfaces
 from opendbc.car.interfaces import CarInterfaceBase, RadarInterfaceBase
 from openpilot.selfdrive.pandad import can_capnp_to_list, can_list_to_can_capnp
 from openpilot.selfdrive.car.cruise import VCruiseHelper
+
+from openpilot.frogpilot.controls.frogpilot_card import FrogPilotCard
 
 REPLAY = "REPLAY" in os.environ
 
@@ -63,6 +65,7 @@ class Car:
   CP: car.CarParams
 
   # FrogPilot variables
+  FPCP: custom.FrogPilotCarParams
 
   def __init__(self, CI=None, RI=None) -> None:
     self.can_sock = messaging.sub_sock('can', timeout=20)
@@ -107,8 +110,9 @@ class Car:
       self.params.put_bool("FirmwareQueryDone", True, block=True)
 
       # FrogPilot variables
+      self.FPCP = self.CI.FPCP
     else:
-      self.CI, self.CP = CI, CI.CP
+      self.CI, self.CP, self.FPCP = CI, CI.CP, CI.FPCP
       self.RI = RI
 
     self.CP.alternativeExperience = 0
@@ -161,15 +165,26 @@ class Car:
     self.rk = Ratekeeper(100, print_delay_threshold=None)
 
     # FrogPilot variables
+    if self.CP.passive:
+      self.FPCP.safetyConfigs = [custom.FrogPilotCarParams.SafetyConfig.new_message()]
 
-  def state_update(self) -> tuple[car.CarState, structs.RadarDataT | None]:
+    fpcp_bytes = self.FPCP.to_bytes()
+    self.params.put("FrogPilotCarParams", fpcp_bytes)
+    self.params.put_nonblocking("FrogPilotCarParamsPersistent", fpcp_bytes)
+
+    self.frogpilot_card = FrogPilotCard(self.CP, self.FPCP)
+
+    self.sm = self.sm.extend(['frogpilotOnroadEvents', 'frogpilotPlan', 'frogpilotUI', 'liveCalibration', 'selfdriveState'])
+    self.pm = self.pm.extend(['frogpilotCarState'])
+
+  def state_update(self) -> tuple[car.CarState, structs.RadarDataT | None, custom.FrogPilotCarState]:
     """carState update loop, driven by can"""
 
     can_strs = messaging.drain_sock_raw(self.can_sock, wait_for_one=True)
     can_list = can_capnp_to_list(can_strs)
 
     # Update carState from CAN
-    CS = self.CI.update(can_list)
+    CS, FPCS = self.CI.update(can_list)
 
     # Update radar tracks from CAN
     RD: structs.RadarDataT | None = self.RI.update(can_list)
@@ -198,10 +213,11 @@ class Car:
     self.v_cruise_helper.update_button_intent(CS)
 
     # FrogPilot variables
+    FPCS = self.frogpilot_card.update(CS, FPCS, self.sm)
 
-    return CS, RD
+    return CS, RD, FPCS
 
-  def state_publish(self, CS: car.CarState, RD: structs.RadarDataT | None):
+  def state_publish(self, CS: car.CarState, RD: structs.RadarDataT | None, FPCS: custom.FrogPilotCarState):
     """carState and carParams publish loop"""
 
     # carParams - logged every 50 seconds (> 1 per segment)
@@ -232,6 +248,10 @@ class Car:
       self.pm.send('liveTracks', tracks_msg)
 
     # FrogPilot variables
+    fpcs_send = messaging.new_message('frogpilotCarState')
+    fpcs_send.valid = CS.canValid
+    fpcs_send.frogpilotCarState = FPCS
+    self.pm.send('frogpilotCarState', fpcs_send)
 
   def controls_update(self, CS: car.CarState, CC: car.CarControl):
     """control update loop, driven by carControl"""
@@ -252,9 +272,9 @@ class Car:
       self.CC_prev = CC
 
   def step(self):
-    CS, RD = self.state_update()
+    CS, RD, FPCS = self.state_update()
 
-    self.state_publish(CS, RD)
+    self.state_publish(CS, RD, FPCS)
 
     initialized = (not any(e.name == EventName.selfdriveInitializing for e in self.sm['onroadEvents']) and
                    self.sm.seen['onroadEvents'])

FrogAi/FrogPilot

selfdrive/car/card.py
@@ -5,7 +5,7 @@ import threading
 
 import cereal.messaging as messaging
 
-from cereal import car, log
+from cereal import car, custom, log
 
 from openpilot.common.params import Params
 from openpilot.common.realtime import config_realtime_process, Priority, Ratekeeper
@@ -19,6 +19,8 @@ from opendbc.car.car_helpers import get_car, interfaces
 from opendbc.car.interfaces import CarInterfaceBase, RadarInterfaceBase
 from openpilot.selfdrive.pandad import can_capnp_to_list, can_list_to_can_capnp
 from openpilot.selfdrive.car.cruise import VCruiseHelper
+
+from openpilot.frogpilot.controls.frogpilot_card import FrogPilotCard
 
 REPLAY = "REPLAY" in os.environ
 
@@ -63,6 +65,7 @@ class Car:
   CP: car.CarParams
 
   # FrogPilot variables
+  FPCP: custom.FrogPilotCarParams
 
   def __init__(self, CI=None, RI=None) -> None:
     self.can_sock = messaging.sub_sock('can', timeout=20)
@@ -107,8 +110,9 @@ class Car:
       self.params.put_bool("FirmwareQueryDone", True, block=True)
 
       # FrogPilot variables
+      self.FPCP = self.CI.FPCP
     else:
-      self.CI, self.CP = CI, CI.CP
+      self.CI, self.CP, self.FPCP = CI, CI.CP, CI.FPCP
       self.RI = RI
 
     self.CP.alternativeExperience = 0
@@ -161,15 +165,26 @@ class Car:
     self.rk = Ratekeeper(100, print_delay_threshold=None)
 
     # FrogPilot variables
+    if self.CP.passive:
+      self.FPCP.safetyConfigs = [custom.FrogPilotCarParams.SafetyConfig.new_message()]
 
-  def state_update(self) -> tuple[car.CarState, structs.RadarDataT | None]:
+    fpcp_bytes = self.FPCP.to_bytes()
+    self.params.put("FrogPilotCarParams", fpcp_bytes)
+    self.params.put("FrogPilotCarParamsPersistent", fpcp_bytes)
+
+    self.frogpilot_card = FrogPilotCard(self.CP, self.FPCP)
+
+    self.sm = self.sm.extend(['frogpilotOnroadEvents', 'frogpilotPlan', 'frogpilotUI', 'liveCalibration', 'selfdriveState'])
+    self.pm = self.pm.extend(['frogpilotCarState'])
+
+  def state_update(self) -> tuple[car.CarState, structs.RadarDataT | None, custom.FrogPilotCarState]:
     """carState update loop, driven by can"""
 
     can_strs = messaging.drain_sock_raw(self.can_sock, wait_for_one=True)
     can_list = can_capnp_to_list(can_strs)
 
     # Update carState from CAN
-    CS = self.CI.update(can_list)
+    CS, FPCS = self.CI.update(can_list)
 
     # Update radar tracks from CAN
     RD: structs.RadarDataT | None = self.RI.update(can_list)
@@ -198,10 +213,11 @@ class Car:
     self.v_cruise_helper.update_button_intent(CS)
 
     # FrogPilot variables
+    FPCS = self.frogpilot_card.update(CS, FPCS, self.sm)
 
-    return CS, RD
+    return CS, RD, FPCS
 
-  def state_publish(self, CS: car.CarState, RD: structs.RadarDataT | None):
+  def state_publish(self, CS: car.CarState, RD: structs.RadarDataT | None, FPCS: custom.FrogPilotCarState):
     """carState and carParams publish loop"""
 
     # carParams - logged every 50 seconds (> 1 per segment)
@@ -232,6 +248,10 @@ class Car:
       self.pm.send('liveTracks', tracks_msg)
 
     # FrogPilot variables
+    fpcs_send = messaging.new_message('frogpilotCarState')
+    fpcs_send.valid = CS.canValid
+    fpcs_send.frogpilotCarState = FPCS
+    self.pm.send('frogpilotCarState', fpcs_send)
 
   def controls_update(self, CS: car.CarState, CC: car.CarControl):
     """control update loop, driven by carControl"""
@@ -252,9 +272,9 @@ class Car:
       self.CC_prev = CC
 
   def step(self):
-    CS, RD = self.state_update()
+    CS, RD, FPCS = self.state_update()
 
-    self.state_publish(CS, RD)
+    self.state_publish(CS, RD, FPCS)
 
     initialized = (not any(e.name == EventName.selfdriveInitializing for e in self.sm['onroadEvents']) and
                    self.sm.seen['onroadEvents'])